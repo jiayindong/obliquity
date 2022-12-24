f906a5fd25e18e9994b7bd62a55af52638e2ff09
showyourwork <showyourwork>
2022-12-24 16:56:40 -0500

    [showyourwork] overleaf sync

--- src/scripts/transform.py
+++ src/scripts/transform.py
@@ -131,300 +131,302 @@ if __name__ == '__main__':
         
         norm3 = pm.sample()
 
-### Make the plot ###
-
-plt.figure(figsize=(7,5),dpi=110)
 
 
-### cosψ ~ Uniform(-1,1) ###
+    ### Make the plot ###
 
-cosψ = uni.posterior.cosψ.values.ravel()
-λ = uni.posterior.λ.values.ravel()
-i = uni.posterior.i.values.ravel()
-iso_cosψ = uni.posterior.iso_cosψ.values.ravel()
-
-plt.subplot(4,4,1)
-plt.hist(cosψ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$\cos{\psi} \sim \mathcal{U}(-1,1)$')
-plt.ylim([0,0.6])
-plt.xlim([-1,1])
-
-plt.subplot(4,4,2)
-plt.hist(λ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$\lambda$')
-plt.ylim([0,0.5])
-plt.xlim([0,np.pi])
-plt.xticks([0,np.pi/2,np.pi], ['0', r'$\pi/2$', r'$\pi$'])
-
-plt.subplot(4,4,3)
-plt.hist(i, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$i_\star$')
-plt.xlim([0,np.pi/2])
-plt.xticks([0,np.pi/4,np.pi/2], ['0', r'$\pi/4$', r'$\pi/2$'])
-plt.ylim([0,1.1])
-
-plt.subplot(4,4,4)
-plt.hist(iso_cosψ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'Inferred $\cos{\psi}$')
-plt.ylim([0,0.6])
-plt.xlim([-1,1])
+    plt.figure(figsize=(7,5),dpi=110)
 
 
-cosψ = np.linspace(-1,1,200)
-pcosψ = np.ones_like(cosψ)/2
+    ### cosψ ~ Uniform(-1,1) ###
 
-λ = np.linspace(0,np.pi,200)
-cosλ = np.cos(λ)
-pcosλ = np.zeros_like(cosλ)
-for i,this_cosλ in enumerate(cosλ):
-    pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5*1/2,
-                         0, 1)[0]*2/np.pi
+    cosψ = uni.posterior.cosψ.values.ravel()
+    λ = uni.posterior.λ.values.ravel()
+    i = uni.posterior.i.values.ravel()
+    iso_cosψ = uni.posterior.iso_cosψ.values.ravel()
 
-istar = np.linspace(0,np.pi/2,800)
-cosi = np.cos(istar)
-pcosi = np.zeros_like(cosi)
-for i,this_cosi in enumerate(cosi):
-    pcosi[i] = integrate.quad(lambda x: this_cosi/x**2/np.sqrt(1-this_cosi**2/x**2)/np.sqrt(1-x**2)*1/2,
-                         this_cosi, 1)[0]*4/np.pi
-    
-plt.subplot(4,4,1)
-plt.plot(cosψ,pcosψ,lw=1.2)
+    plt.subplot(4,4,1)
+    plt.hist(cosψ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$\cos{\psi} \sim \mathcal{U}(-1,1)$')
+    plt.ylim([0,0.6])
+    plt.xlim([-1,1])
 
-plt.subplot(4,4,2)
-plt.plot(np.arccos(cosλ),pcosλ*np.sqrt(1-cosλ**2),lw=1.2)
+    plt.subplot(4,4,2)
+    plt.hist(λ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$\lambda$')
+    plt.ylim([0,0.5])
+    plt.xlim([0,np.pi])
+    plt.xticks([0,np.pi/2,np.pi], ['0', r'$\pi/2$', r'$\pi$'])
 
-plt.subplot(4,4,3)
-plt.plot(np.arccos(cosi), pcosi*np.sqrt(1-cosi**2),lw=1.2)
+    plt.subplot(4,4,3)
+    plt.hist(i, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$i_\star$')
+    plt.xlim([0,np.pi/2])
+    plt.xticks([0,np.pi/4,np.pi/2], ['0', r'$\pi/4$', r'$\pi/2$'])
+    plt.ylim([0,1.1])
 
-plt.subplot(4,4,4)
-plt.plot(cosψ,pcosψ,lw=1.2,linestyle='--',label='True')
-plt.legend(framealpha=0,handlelength=1,handletextpad=0.3,borderpad=0.1)
-
-
-### cosψ ~ Normal(0,0.2) ###
-
-σ = 0.2
-
-cosψ = norm1.posterior.cosψ.values.ravel()
-λ = norm1.posterior.λ.values.ravel()
-i = norm1.posterior.i.values.ravel()
-iso_cosψ = norm1.posterior.iso_cosψ.values.ravel()
-
-plt.subplot(4,4,5)
-plt.hist(cosψ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$\cos{\psi} \sim \mathcal{N}(0,0.2)$')
-plt.ylim([0,2.5])
-plt.xlim([-1,1])
-
-plt.subplot(4,4,6)
-plt.hist(λ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$\lambda$')
-plt.ylim([0,1.5])
-plt.xlim([0,np.pi])
-plt.xticks([0,np.pi/2,np.pi], ['0', r'$\pi/2$', r'$\pi$'])
-
-plt.subplot(4,4,7)
-plt.hist(i, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$i_\star$')
-plt.xlim([0,np.pi/2])
-plt.xticks([0,np.pi/4,np.pi/2], ['0', r'$\pi/4$', r'$\pi/2$'])
-plt.ylim([0,1.1])
-
-plt.subplot(4,4,8)
-plt.hist(iso_cosψ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'Inferred $\cos{\psi}$')
-plt.ylim([0,2.5])
-plt.xlim([-1,1])
-
-cosψ = np.linspace(-1,1,200)
-pcosψ = 1/np.sqrt(2*np.pi*σ**2)*np.exp(-cosψ**2/2/σ**2)
-
-λ = np.linspace(0,np.pi,200)
-cosλ = np.cos(λ)
-pcosλ = np.zeros_like(cosλ)
-for i,this_cosλ in enumerate(cosλ):
-    pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5
-                              *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(this_cosλ**2*x**2-this_cosλ**2)/(this_cosλ**2*x**2-1)/2/σ**2),
-                         0, 1)[0]*2/np.pi
-
-istar = np.linspace(0,np.pi/2,800)
-cosi = np.cos(istar)
-pcosi = np.zeros_like(cosi)
-for i,this_cosi in enumerate(cosi):
-    pcosi[i] = integrate.quad(lambda x: this_cosi/x**2/np.sqrt(1-this_cosi**2/x**2)/np.sqrt(1-x**2)
-                              *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(1-this_cosi**2/x**2)/2/σ**2),
-                         this_cosi, 1)[0]*4/np.pi
-
-plt.subplot(4,4,5)
-plt.plot(cosψ,pcosψ,lw=1.2)
-
-plt.subplot(4,4,6)
-plt.plot(λ,pcosλ*np.sqrt(1-cosλ**2),lw=1.2)
-
-plt.subplot(4,4,7)
-plt.plot(istar, pcosi*np.sqrt(1-cosi**2),lw=1.2)
-
-plt.subplot(4,4,8)
-plt.plot(cosψ,pcosψ,lw=1.2,linestyle='--',label='True')
-plt.legend(framealpha=0,handlelength=1,handletextpad=0.3,borderpad=0.1)
+    plt.subplot(4,4,4)
+    plt.hist(iso_cosψ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'Inferred $\cos{\psi}$')
+    plt.ylim([0,0.6])
+    plt.xlim([-1,1])
 
 
+    cosψ = np.linspace(-1,1,200)
+    pcosψ = np.ones_like(cosψ)/2
 
-### cosψ ~ Normal(-0.4,0.2) ###
+    λ = np.linspace(0,np.pi,200)
+    cosλ = np.cos(λ)
+    pcosλ = np.zeros_like(cosλ)
+    for i,this_cosλ in enumerate(cosλ):
+        pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5*1/2,
+                             0, 1)[0]*2/np.pi
 
-cosψ = norm2.posterior.cosψ.values.ravel()
-λ = norm2.posterior.λ.values.ravel()
-i = norm2.posterior.i.values.ravel()
-iso_cosψ = norm2.posterior.iso_cosψ.values.ravel()
+    istar = np.linspace(0,np.pi/2,800)
+    cosi = np.cos(istar)
+    pcosi = np.zeros_like(cosi)
+    for i,this_cosi in enumerate(cosi):
+        pcosi[i] = integrate.quad(lambda x: this_cosi/x**2/np.sqrt(1-this_cosi**2/x**2)/np.sqrt(1-x**2)*1/2,
+                             this_cosi, 1)[0]*4/np.pi
+        
+    plt.subplot(4,4,1)
+    plt.plot(cosψ,pcosψ,lw=1.2)
+
+    plt.subplot(4,4,2)
+    plt.plot(np.arccos(cosλ),pcosλ*np.sqrt(1-cosλ**2),lw=1.2)
+
+    plt.subplot(4,4,3)
+    plt.plot(np.arccos(cosi), pcosi*np.sqrt(1-cosi**2),lw=1.2)
+
+    plt.subplot(4,4,4)
+    plt.plot(cosψ,pcosψ,lw=1.2,linestyle='--',label='True')
+    plt.legend(framealpha=0,handlelength=1,handletextpad=0.3,borderpad=0.1)
 
 
-plt.subplot(4,4,9)
-plt.hist(cosψ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$\cos{\psi} \sim \mathcal{N}(-0.4,0.2)$')
-plt.ylim([0,2.5])
-plt.xlim([-1,1])
+    ### cosψ ~ Normal(0,0.2) ###
 
-plt.subplot(4,4,10)
-plt.hist(λ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$\lambda$')
-plt.ylim([0,1.5])
-plt.xlim([0,np.pi])
-plt.xticks([0,np.pi/2,np.pi], ['0', r'$\pi/2$', r'$\pi$'])
+    σ = 0.2
 
-plt.subplot(4,4,11)
-plt.hist(i, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$i_\star$')
-plt.xlim([0,np.pi/2])
-plt.xticks([0,np.pi/4,np.pi/2], ['0', r'$\pi/4$', r'$\pi/2$'])
-plt.ylim([0,1.1])
+    cosψ = norm1.posterior.cosψ.values.ravel()
+    λ = norm1.posterior.λ.values.ravel()
+    i = norm1.posterior.i.values.ravel()
+    iso_cosψ = norm1.posterior.iso_cosψ.values.ravel()
 
-plt.subplot(4,4,12)
-plt.hist(iso_cosψ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'Inferred $\cos{\psi}$')
-plt.ylim([0,2.5])
-plt.xlim([-1,1])
+    plt.subplot(4,4,5)
+    plt.hist(cosψ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$\cos{\psi} \sim \mathcal{N}(0,0.2)$')
+    plt.ylim([0,2.5])
+    plt.xlim([-1,1])
 
-cosψ = np.linspace(-1,1,200)
-pcosψ = 1/np.sqrt(2*np.pi*σ**2)*np.exp(-(cosψ+0.4)**2/2/σ**2)
+    plt.subplot(4,4,6)
+    plt.hist(λ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$\lambda$')
+    plt.ylim([0,1.5])
+    plt.xlim([0,np.pi])
+    plt.xticks([0,np.pi/2,np.pi], ['0', r'$\pi/2$', r'$\pi$'])
 
-λ = np.linspace(0,np.pi/2,200)
-cosλ = np.cos(λ)
-pcosλ = np.zeros_like(cosλ)
-for i,this_cosλ in enumerate(cosλ):
-    pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5
-                              *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt((this_cosλ**2*x**2-this_cosλ**2)/(this_cosλ**2*x**2-1))+0.4)**2/2/σ**2),
-                         0, 1)[0]*2/np.pi
-    
-plt.subplot(4,4,10)
-plt.plot(λ,pcosλ*np.sqrt(1-cosλ**2),lw=1.2)
+    plt.subplot(4,4,7)
+    plt.hist(i, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$i_\star$')
+    plt.xlim([0,np.pi/2])
+    plt.xticks([0,np.pi/4,np.pi/2], ['0', r'$\pi/4$', r'$\pi/2$'])
+    plt.ylim([0,1.1])
 
-λ = np.linspace(np.pi/2,3*np.pi/2,200)
-cosλ = np.cos(λ)
-pcosλ = np.zeros_like(cosλ)
-for i,this_cosλ in enumerate(cosλ):
-    pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5
-                              *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt((this_cosλ**2*x**2-this_cosλ**2)/(this_cosλ**2*x**2-1))-0.4)**2/2/σ**2),
-                         0, 1)[0]*2/np.pi
+    plt.subplot(4,4,8)
+    plt.hist(iso_cosψ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'Inferred $\cos{\psi}$')
+    plt.ylim([0,2.5])
+    plt.xlim([-1,1])
 
-plt.plot(λ,pcosλ*np.sqrt(1-cosλ**2), c='C0',lw=1.2)
-    
-istar = np.linspace(0,np.pi/2,800)
-cosi = np.cos(istar)
-pcosi = np.zeros_like(cosi)
-for i,this_cosi in enumerate(cosi):
-    pcosi[i] = integrate.quad(lambda x: this_cosi/x**2/np.sqrt(1-this_cosi**2/x**2)/np.sqrt(1-x**2)
-                              *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt(1-this_cosi**2/x**2)-0.4)**2/2/σ**2),
-                         this_cosi, 1)[0]*4/np.pi
+    cosψ = np.linspace(-1,1,200)
+    pcosψ = 1/np.sqrt(2*np.pi*σ**2)*np.exp(-cosψ**2/2/σ**2)
 
-plt.subplot(4,4,9)
-plt.plot(cosψ,pcosψ,lw=1.2)
+    λ = np.linspace(0,np.pi,200)
+    cosλ = np.cos(λ)
+    pcosλ = np.zeros_like(cosλ)
+    for i,this_cosλ in enumerate(cosλ):
+        pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5
+                                  *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(this_cosλ**2*x**2-this_cosλ**2)/(this_cosλ**2*x**2-1)/2/σ**2),
+                             0, 1)[0]*2/np.pi
 
-plt.subplot(4,4,11)
-plt.plot(istar, pcosi*np.sqrt(1-cosi**2)/2,lw=1.2)
+    istar = np.linspace(0,np.pi/2,800)
+    cosi = np.cos(istar)
+    pcosi = np.zeros_like(cosi)
+    for i,this_cosi in enumerate(cosi):
+        pcosi[i] = integrate.quad(lambda x: this_cosi/x**2/np.sqrt(1-this_cosi**2/x**2)/np.sqrt(1-x**2)
+                                  *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(1-this_cosi**2/x**2)/2/σ**2),
+                             this_cosi, 1)[0]*4/np.pi
 
-plt.subplot(4,4,12)
-plt.plot(cosψ,pcosψ,lw=1.2,linestyle='--',label='True')
-plt.legend(framealpha=0,handlelength=1,handletextpad=0.3,borderpad=0.1)
+    plt.subplot(4,4,5)
+    plt.plot(cosψ,pcosψ,lw=1.2)
+
+    plt.subplot(4,4,6)
+    plt.plot(λ,pcosλ*np.sqrt(1-cosλ**2),lw=1.2)
+
+    plt.subplot(4,4,7)
+    plt.plot(istar, pcosi*np.sqrt(1-cosi**2),lw=1.2)
+
+    plt.subplot(4,4,8)
+    plt.plot(cosψ,pcosψ,lw=1.2,linestyle='--',label='True')
+    plt.legend(framealpha=0,handlelength=1,handletextpad=0.3,borderpad=0.1)
 
 
 
-### cosψ ~ Normal(0.4,0.2) ###
+    ### cosψ ~ Normal(-0.4,0.2) ###
 
-cosψ = norm3.posterior.cosψ.values.ravel()
-λ = norm3.posterior.λ.values.ravel()
-i = norm3.posterior.i.values.ravel()
-iso_cosψ = norm3.posterior.iso_cosψ.values.ravel()
+    cosψ = norm2.posterior.cosψ.values.ravel()
+    λ = norm2.posterior.λ.values.ravel()
+    i = norm2.posterior.i.values.ravel()
+    iso_cosψ = norm2.posterior.iso_cosψ.values.ravel()
 
-plt.subplot(4,4,13)
-plt.hist(cosψ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$\cos{\psi} \sim \mathcal{N}(0.4,0.2)$')
-plt.ylim([0,2.5])
-plt.xlim([-1,1])
 
-plt.subplot(4,4,14)
-plt.hist(λ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$\lambda$')
-plt.ylim([0,1.5])
-plt.xlim([0,np.pi])
-plt.xticks([0,np.pi/2,np.pi], ['0', r'$\pi/2$', r'$\pi$'])
+    plt.subplot(4,4,9)
+    plt.hist(cosψ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$\cos{\psi} \sim \mathcal{N}(-0.4,0.2)$')
+    plt.ylim([0,2.5])
+    plt.xlim([-1,1])
 
-plt.subplot(4,4,15)
-plt.hist(i, color='#e5e1e0', density=True, bins=40)
-plt.title(r'$i_\star$')
-plt.xlim([0,np.pi/2])
-plt.xticks([0,np.pi/4,np.pi/2], ['0', r'$\pi/4$', r'$\pi/2$'])
-plt.ylim([0,1.1])
+    plt.subplot(4,4,10)
+    plt.hist(λ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$\lambda$')
+    plt.ylim([0,1.5])
+    plt.xlim([0,np.pi])
+    plt.xticks([0,np.pi/2,np.pi], ['0', r'$\pi/2$', r'$\pi$'])
 
-plt.subplot(4,4,16)
-plt.hist(iso_cosψ, color='#e5e1e0', density=True, bins=40)
-plt.title(r'Inferred $\cos{\psi}$')
-plt.ylim([0,2.5])
-plt.xlim([-1,1])
+    plt.subplot(4,4,11)
+    plt.hist(i, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$i_\star$')
+    plt.xlim([0,np.pi/2])
+    plt.xticks([0,np.pi/4,np.pi/2], ['0', r'$\pi/4$', r'$\pi/2$'])
+    plt.ylim([0,1.1])
 
-cosψ = np.linspace(-1,1,200)
-pcosψ = 1/np.sqrt(2*np.pi*σ**2)*np.exp(-(cosψ-0.4)**2/2/σ**2)
+    plt.subplot(4,4,12)
+    plt.hist(iso_cosψ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'Inferred $\cos{\psi}$')
+    plt.ylim([0,2.5])
+    plt.xlim([-1,1])
 
-λ = np.linspace(0,np.pi/2,200)
-cosλ = np.cos(λ)
-pcosλ = np.zeros_like(cosλ)
-for i,this_cosλ in enumerate(cosλ):
-    pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5
-                              *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt((this_cosλ**2*x**2-this_cosλ**2)/(this_cosλ**2*x**2-1))-0.4)**2/2/σ**2),
-                         0, 1)[0]*2/np.pi
-    
-plt.subplot(4,4,14)
-plt.plot(λ,pcosλ*np.sqrt(1-cosλ**2),lw=1.2)
+    cosψ = np.linspace(-1,1,200)
+    pcosψ = 1/np.sqrt(2*np.pi*σ**2)*np.exp(-(cosψ+0.4)**2/2/σ**2)
 
-λ = np.linspace(np.pi/2,3*np.pi/2,200)
-cosλ = np.cos(λ)
-pcosλ = np.zeros_like(cosλ)
-for i,this_cosλ in enumerate(cosλ):
-    pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5
-                              *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt((this_cosλ**2*x**2-this_cosλ**2)/(this_cosλ**2*x**2-1))+0.4)**2/2/σ**2),
-                         0, 1)[0]*2/np.pi
+    λ = np.linspace(0,np.pi/2,200)
+    cosλ = np.cos(λ)
+    pcosλ = np.zeros_like(cosλ)
+    for i,this_cosλ in enumerate(cosλ):
+        pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5
+                                  *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt((this_cosλ**2*x**2-this_cosλ**2)/(this_cosλ**2*x**2-1))+0.4)**2/2/σ**2),
+                             0, 1)[0]*2/np.pi
+        
+    plt.subplot(4,4,10)
+    plt.plot(λ,pcosλ*np.sqrt(1-cosλ**2),lw=1.2)
 
-plt.plot(λ,pcosλ*np.sqrt(1-cosλ**2),c='C0',lw=1.2)
-    
-istar = np.linspace(0,np.pi/2,800)
-cosi = np.cos(istar)
-pcosi = np.zeros_like(cosi)
-for i,this_cosi in enumerate(cosi):
-    pcosi[i] = integrate.quad(lambda x: this_cosi/x**2/np.sqrt(1-this_cosi**2/x**2)/np.sqrt(1-x**2)
-                              *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt(1-this_cosi**2/x**2)-0.4)**2/2/σ**2),
-                         this_cosi, 1)[0]*4/np.pi
+    λ = np.linspace(np.pi/2,3*np.pi/2,200)
+    cosλ = np.cos(λ)
+    pcosλ = np.zeros_like(cosλ)
+    for i,this_cosλ in enumerate(cosλ):
+        pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5
+                                  *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt((this_cosλ**2*x**2-this_cosλ**2)/(this_cosλ**2*x**2-1))-0.4)**2/2/σ**2),
+                             0, 1)[0]*2/np.pi
 
-plt.subplot(4,4,13)
-plt.plot(cosψ,pcosψ,lw=1.2)
+    plt.plot(λ,pcosλ*np.sqrt(1-cosλ**2), c='C0',lw=1.2)
+        
+    istar = np.linspace(0,np.pi/2,800)
+    cosi = np.cos(istar)
+    pcosi = np.zeros_like(cosi)
+    for i,this_cosi in enumerate(cosi):
+        pcosi[i] = integrate.quad(lambda x: this_cosi/x**2/np.sqrt(1-this_cosi**2/x**2)/np.sqrt(1-x**2)
+                                  *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt(1-this_cosi**2/x**2)-0.4)**2/2/σ**2),
+                             this_cosi, 1)[0]*4/np.pi
 
-plt.subplot(4,4,15)
-plt.plot(istar, pcosi*np.sqrt(1-cosi**2)/2,lw=1.2)
+    plt.subplot(4,4,9)
+    plt.plot(cosψ,pcosψ,lw=1.2)
 
-plt.subplot(4,4,16)
-plt.plot(cosψ,pcosψ,lw=1.2,linestyle='--',label='True')
-plt.legend(framealpha=0,handlelength=1,handletextpad=0.3,borderpad=0.1)
+    plt.subplot(4,4,11)
+    plt.plot(istar, pcosi*np.sqrt(1-cosi**2)/2,lw=1.2)
 
-plt.tight_layout()
-plt.show()
+    plt.subplot(4,4,12)
+    plt.plot(cosψ,pcosψ,lw=1.2,linestyle='--',label='True')
+    plt.legend(framealpha=0,handlelength=1,handletextpad=0.3,borderpad=0.1)
 
-plt.savefig(paths.figures / "transform.pdf", bbox_inches="tight", dpi=600)
-plt.close()
+
+
+    ### cosψ ~ Normal(0.4,0.2) ###
+
+    cosψ = norm3.posterior.cosψ.values.ravel()
+    λ = norm3.posterior.λ.values.ravel()
+    i = norm3.posterior.i.values.ravel()
+    iso_cosψ = norm3.posterior.iso_cosψ.values.ravel()
+
+    plt.subplot(4,4,13)
+    plt.hist(cosψ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$\cos{\psi} \sim \mathcal{N}(0.4,0.2)$')
+    plt.ylim([0,2.5])
+    plt.xlim([-1,1])
+
+    plt.subplot(4,4,14)
+    plt.hist(λ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$\lambda$')
+    plt.ylim([0,1.5])
+    plt.xlim([0,np.pi])
+    plt.xticks([0,np.pi/2,np.pi], ['0', r'$\pi/2$', r'$\pi$'])
+
+    plt.subplot(4,4,15)
+    plt.hist(i, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'$i_\star$')
+    plt.xlim([0,np.pi/2])
+    plt.xticks([0,np.pi/4,np.pi/2], ['0', r'$\pi/4$', r'$\pi/2$'])
+    plt.ylim([0,1.1])
+
+    plt.subplot(4,4,16)
+    plt.hist(iso_cosψ, color='#e5e1e0', density=True, bins=40)
+    plt.title(r'Inferred $\cos{\psi}$')
+    plt.ylim([0,2.5])
+    plt.xlim([-1,1])
+
+    cosψ = np.linspace(-1,1,200)
+    pcosψ = 1/np.sqrt(2*np.pi*σ**2)*np.exp(-(cosψ-0.4)**2/2/σ**2)
+
+    λ = np.linspace(0,np.pi/2,200)
+    cosλ = np.cos(λ)
+    pcosλ = np.zeros_like(cosλ)
+    for i,this_cosλ in enumerate(cosλ):
+        pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5
+                                  *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt((this_cosλ**2*x**2-this_cosλ**2)/(this_cosλ**2*x**2-1))-0.4)**2/2/σ**2),
+                             0, 1)[0]*2/np.pi
+        
+    plt.subplot(4,4,14)
+    plt.plot(λ,pcosλ*np.sqrt(1-cosλ**2),lw=1.2)
+
+    λ = np.linspace(np.pi/2,3*np.pi/2,200)
+    cosλ = np.cos(λ)
+    pcosλ = np.zeros_like(cosλ)
+    for i,this_cosλ in enumerate(cosλ):
+        pcosλ[i] = integrate.quad(lambda x: (1-this_cosλ**2*x**2)**-1.5
+                                  *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt((this_cosλ**2*x**2-this_cosλ**2)/(this_cosλ**2*x**2-1))+0.4)**2/2/σ**2),
+                             0, 1)[0]*2/np.pi
+
+    plt.plot(λ,pcosλ*np.sqrt(1-cosλ**2),c='C0',lw=1.2)
+        
+    istar = np.linspace(0,np.pi/2,800)
+    cosi = np.cos(istar)
+    pcosi = np.zeros_like(cosi)
+    for i,this_cosi in enumerate(cosi):
+        pcosi[i] = integrate.quad(lambda x: this_cosi/x**2/np.sqrt(1-this_cosi**2/x**2)/np.sqrt(1-x**2)
+                                  *1/np.sqrt(2*np.pi*σ**2)*np.exp(-(np.sqrt(1-this_cosi**2/x**2)-0.4)**2/2/σ**2),
+                             this_cosi, 1)[0]*4/np.pi
+
+    plt.subplot(4,4,13)
+    plt.plot(cosψ,pcosψ,lw=1.2)
+
+    plt.subplot(4,4,15)
+    plt.plot(istar, pcosi*np.sqrt(1-cosi**2)/2,lw=1.2)
+
+    plt.subplot(4,4,16)
+    plt.plot(cosψ,pcosψ,lw=1.2,linestyle='--',label='True')
+    plt.legend(framealpha=0,handlelength=1,handletextpad=0.3,borderpad=0.1)
+
+    plt.tight_layout()
+    plt.show()
+
+    plt.savefig(paths.figures / "transform.pdf", bbox_inches="tight", dpi=600)
+    plt.close()
