8c0e151c80b9c84df43cc8facf56b8847fb7360a
jiayindong <jdong@flatironinstitute.org>
2023-05-21 17:44:50 -0400

    add polar plots

--- src/scripts/polar.py
+++ src/scripts/polar.py
@@ -98,8 +98,6 @@ if __name__ == '__main__':
 	# Subsample with Prot measurements
 	nsample2 = int(np.sum(mask2))
 
-	print('Sample size': nsample)
-
 	sim_dir = paths.data / "polar"
 	sim_dir.mkdir(exist_ok=True, parents=True)
 
